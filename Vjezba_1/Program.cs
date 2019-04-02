e17f2fa02d57fe093d428714e07ecd0a7fd18753
ShobeMasterRace <48776393+ShobeMasterRace@users.noreply.github.com>
2019-04-02 20:58:18 +0200

    Add files via upload

---
+++ Vjezba_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Vjezba_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TextWriter tw = new StreamWriter("D:/tekst.txt", true);
+                tw.WriteLine("hecking bamboozle fren");
+                tw.Close();
+                Console.WriteLine("File created");
+            }
+            catch (Exception e)
+            {
+                Console.Write(e);
+
+            }
+
+            Console.Read();
+        }
+    }
+}
