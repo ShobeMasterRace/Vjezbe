0635ce7af2c558841de40aa824def1343abe1580
ShobeMasterRace <48776393+ShobeMasterRace@users.noreply.github.com>
2019-04-09 02:16:58 +0200

    Add files via upload

---
+++ Zadatak_4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Zadatak_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(-1 + 4 * 6);
+            System.Console.WriteLine((35 + 5) % 7);
+            System.Console.WriteLine(14 + -4 * 6 / 11);
+            System.Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Zadatak_5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Zadatak_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int fn, sn, swoosh;
+            Console.WriteLine("Upiši prvi broj: ");
+            fn = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Upiši drugi broj: ");
+            sn = int.Parse(Console.ReadLine());
+
+            swoosh = fn;
+            fn = sn;
+            sn = swoosh;
+
+            
+            Console.WriteLine("\nSad je prvi broj: " + fn);
+            Console.WriteLine("Sad je drugi broj: " + sn);
+
+
+
+
+            Console.Read();
+
+            
+        }
+    }
+}
