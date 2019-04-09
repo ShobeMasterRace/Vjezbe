0d3a46983943eb4f227d91b67d1f84eb4f297a5b
ShobeMasterRace <48776393+ShobeMasterRace@users.noreply.github.com>
2019-04-09 02:20:55 +0200

    Add files via upload

---
+++ Zadatak_10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Zadatak_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fn, sn, tn;
+
+            Console.WriteLine("Prvi Broj?");
+            fn = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Drugi Broj?");
+            sn = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Treci Broj?");
+            tn = int.Parse(Console.ReadLine());
+
+            int rezjed = (fn + sn) * tn;
+            int rezdva = fn * sn + sn * tn;
+
+            Console.WriteLine("Rezultati su sljedeći: " + rezjed + " || " + rezdva);
+
+            Console.Read();
+
+        }
+    }
+}
