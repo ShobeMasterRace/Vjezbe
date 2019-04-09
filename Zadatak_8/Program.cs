14f4a2c167e9c72c752883ddfc9ce8887ca2e5a3
ShobeMasterRace <48776393+ShobeMasterRace@users.noreply.github.com>
2019-04-09 02:19:43 +0200

    Add files via upload

---
+++ Zadatak_8/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Zadatak_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Seksi tablica mnozenja
+            int fn, jen, dva, tri, cetri, pet, sest, sedam, osam, devet, deset;
+
+            Console.WriteLine("Unesi jebeni broj: ");
+            fn = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nEvo ti na");
+
+            jen = fn * 1;
+            dva = fn * 2;
+            tri = fn * 3;
+            cetri = fn * 4;
+            pet = fn * 5;
+            sest = fn * 6;
+            sedam = fn * 7;
+            osam = fn * 8;
+            devet = fn * 9;
+            deset = fn * 10;
+
+            Console.WriteLine(jen);
+            Console.WriteLine(dva);
+            Console.WriteLine(tri);
+            Console.WriteLine(cetri);
+            Console.WriteLine(pet);
+            Console.WriteLine(sest);
+            Console.WriteLine(sedam);
+            Console.WriteLine(osam);
+            Console.WriteLine(devet);
+            Console.WriteLine(deset);
+
+            //superpro
+
+            Console.Read();
+
+
+        }
+    }
+}
---
+++ Zadatak_9/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Zadatak_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //kalkulacije prosjeka
+            int fn, sn, tn, ffn;
+            Console.WriteLine("Upiši prvi broj: ");
+            fn = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Upiši drugi broj: ");
+            sn = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Upiši treci broj: ");
+            tn = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Upiši cetvrti broj: ");
+            ffn = int.Parse(Console.ReadLine());
+
+
+            int evrd = (fn + sn + tn + ffn) / 4;
+            
+            Console.WriteLine("Evrđ je: " + evrd );
+
+
+
+
+            Console.Read();
+
+
+
+
+         
+        }
+    }
+}
