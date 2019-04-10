d004c2cdc4e94cee2d5d8f45e3ac94a97741ebf0
ShobeMasterRace <48776393+ShobeMasterRace@users.noreply.github.com>
2019-04-10 02:20:07 +0200

    Add files via upload

---
+++ Zadatak_14/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Zadatak_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ft, far;
+            double cel;
+            Console.WriteLine("Kolika je temperatura??");
+
+            ft = int.Parse(Console.ReadLine());
+
+            cel = ft + 273.15;
+            Console.WriteLine("Celzij: " + cel);
+
+            far = (ft * 9 / 5) + 32;
+            Console.WriteLine("Ferenhejt: " + far);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Zadatak_15/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Zadatak_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string bambo;
+
+            string remove(string tks, int br)
+            {
+
+                return tks.Remove(br, 1);
+
+            }
+
+            Console.WriteLine("Upiši štagod ali bar da ima 5 slova ");
+            bambo = Console.ReadLine();
+            
+            {
+
+                Console.WriteLine(remove(bambo, 1));
+                Console.WriteLine(remove(bambo, 4));
+                Console.WriteLine(remove(bambo, 0));
+
+            }
+            
+            //napravio sam više nego šta je trebalo za zadatak :P 
+
+            Console.ReadLine();
+        }
+    }
+}
