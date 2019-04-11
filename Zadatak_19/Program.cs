2fec49805e67f366e0b4d892287649a08f1f426b
ShobeMasterRace <48776393+ShobeMasterRace@users.noreply.github.com>
2019-04-11 03:34:03 +0200

    Add files via upload

---
+++ Zadatak_19/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Zadatak_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First HIV");
+            int fn = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Second HIV");
+            int sn = Convert.ToInt32(Console.ReadLine());
+            
+            //ToInt32 - konvertira u 32-bit integer
+
+
+            Console.WriteLine("Is the first number HIV positive and the second HIV negative??");
+            Console.WriteLine((fn < 0 && sn > 0) || (fn > 0 && sn < 0));
+
+
+            // && znači "i", x i y moraju bit 'true' , u protivnom je 'false' 
+            // || znači "ili" 
+
+            Console.ReadLine();
+        }
+    }
+}
